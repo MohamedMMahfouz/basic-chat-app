a4614920f2a0f4d8513cc3ff2af5a8493a0b87ad
mohamedmmahfouz <mohamed.abdel-moneam@gmail.com>
2021-01-30 15:54:24 +0200

    add some indices for query optimizations

--- db/migrate/20210128111751_create_chats.rb
+++ db/migrate/20210128111751_create_chats.rb
@@ -2,7 +2,7 @@ class CreateChats < ActiveRecord::Migration[5.2]
   def change
     create_table :chats do |t|
       t.references :application, foreign_key: true
-      t.integer :number
+      t.integer :number, index: true
       t.integer :messages_count, default: 0
 
       t.timestamps
--- db/migrate/20210128112533_create_messages.rb
+++ db/migrate/20210128112533_create_messages.rb
@@ -3,7 +3,7 @@ class CreateMessages < ActiveRecord::Migration[5.2]
     create_table :messages do |t|
       t.text :content
       t.references :chat, foreign_key: true
-      t.integer :number
+      t.integer :number, index: true
 
       t.timestamps
     end
